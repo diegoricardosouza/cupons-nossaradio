899e47b97019dc318844c4b0fe5fb75ee204dbac
Diego Ricardo <diegoricardoweb@gmail.com>
2022-05-19 20:41:06 -0300

    Adicionado estilos pagina de captura de emails

--- resources/views/front/city.blade.php
+++ resources/views/front/city.blade.php
@@ -8,8 +8,26 @@
 
 
 @section('content')
-
 <div class="container">
+    <div class="coupons-modal__bg">
+        <div class="coupons-modal__content">
+            <div class="coupons-modal__form">
+                <img src="{{ url('images/logo.png') }}" alt="" class="img-form">
+
+                <div>
+                    <form action="" method="post">
+                        @csrf
+                        <input type="email" name="email" required placeholder="e-mail">
+                        <button type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 103.44" style="enable-background:new 0 0 122.88 103.44" xml:space="preserve"><path d="M69.49 102.77 49.8 84.04l-20.23 18.27c-.45.49-1.09.79-1.8.79-1.35 0-2.44-1.09-2.44-2.44V60.77L.76 37.41a2.445 2.445 0 0 1-.09-3.45c.31-.33.7-.55 1.11-.67l118-33.2a2.444 2.444 0 0 1 2.77 3.58l-49.2 98.42c-.6 1.2-2.06 1.69-3.26 1.09-.23-.11-.43-.25-.6-.41zM46.26 80.68 30.21 65.42v29.76l16.05-14.5zM28.15 56.73l76.32-47.26L7.22 36.83l20.93 19.9zm86.28-47.7L31.79 60.19l38.67 36.78 43.97-87.94z"/></svg>
+                        </button>
+                    </form>
+
+                    <span class="info">CADASTRE SEU EMAIL E ESCOLHA SEUS CUPONS</span>
+                </div>
+            </div>
+        </div>
+
         @if($coupons)
         <div class="row">
             <div class="col-lg-12 coupons__title">
@@ -51,6 +69,7 @@
         </div>
         @endif
     </div>
+</div>
 @endsection
 
 @section('scripts')
